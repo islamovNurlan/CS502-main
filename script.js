1323187480abb7cf3ee3e096bcfdc331169fdf07
islamovNurlan <nurlanislamov155@gmail.com>
2023-07-29 20:04:48 +0400

    New

---
+++ script.js
@@ -0,0 +1,119 @@
+// const username = document.querySelector("#username")
+// const email = document.querySelector("#email")
+// const password = document.querySelector("#password")
+// const form = document.querySelector("#form")
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     const obj = {
+//         username: username.value,
+//         email: email.value,
+//         password: password.value
+//     }
+
+//     alert(`Hi, ${obj.username}`)
+//     console.log(obj);
+// })
+
+//Variables, operators, if else switch, data type
+
+// there are three way to declare variables: const, let, var + auto
+// +,-,/,=,*,%,**,==,===
+// let a, b
+// a = 5
+// b = 3
+// let z = a % b
+// console.log(z);
+
+// let students = 4
+
+// students += students
+// students = students * students
+
+// console.log(students);
+
+// if (false) {
+//     console.log('true');
+// } else if (false) {
+//     console.log('9');
+// } else {
+//     console.log('false');
+// }
+
+// let num = 4
+// switch (num) {
+//     case 4:
+//         console.log('4');
+//         break;
+//     case 6:
+//         console.log('6');
+//         break;
+//     default:
+//         console.log('not found');
+//         break;
+
+// }
+
+
+// primitive data type: 7
+// string,number,bigInt,boolean,symbol,null,undefind
+
+// string  -string
+// number -number
+// bigInt -number
+// boolean - boolean
+// Symbol - function
+// null - object
+// undefined  -undefined
+
+//  non-primitive data type: 1
+// object
+
+// Object
+// Array
+// Date
+
+// let const var - keywords
+// firstName- variable
+// Ferid -value
+// typeof string
+
+// const firstName1 = "Ferid" //string
+// console.log(firstName1);
+
+// let age = 25 //number, bigInt
+// let isAdmin = true //boolean
+// let salary = undefined //undefined
+// let surname = null //null
+// let city = Symbol() //Symbol
+
+// let obj = {} //object
+// let arr = [] //array
+
+// let time = new Date()
+
+// == vs ===
+// == just value
+// === value and type
+
+// let num = '77' //string
+// let num2 = '77' //number
+
+// console.log(num == num2); //true
+// console.log(num === num2); //true
+
+// console.log(typeof num);
+// console.log(typeof num2);
+
+// || vs &&
+
+// let firstName = 'Ferid'
+// let surname = "Feridli"
+// let age = 23
+
+// && > ||
+
+// if (age > 30 || surname == "Feridli" && firstName == "Ferid") {
+//     console.log(firstName, age);
+// }
+
