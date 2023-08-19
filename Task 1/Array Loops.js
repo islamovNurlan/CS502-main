ce14369803aa59a808a959a3fddc1aaf5882b5b0
islamovNurlan <nurlanislamov155@gmail.com>
2023-08-19 15:40:50 +0400

    new

--- Task 1/Array Loops.js
+++ Task 1/Array Loops.js
@@ -1,38 +1,38 @@
-// // let fullName = 'Ulfat Zakirli Rovshen'
+// let fullName = 'Ulfat Zakirli Rovshen'
 
-// // 1) Ad soyad ata adinizi butov sheklde goturun ve array sheklinde herflerine ayirin(split)
+// 1) Ad soyad ata adinizi butov sheklde goturun ve array sheklinde herflerine ayirin(split)
 
-// // let fullName = "Nurlan Islamov Nizami";
-// // let lettersArray = fullName.split("");
-// // console.log(lettersArray);
+// let fullName = "Nurlan Islamov Nizami";
+// let lettersArray = fullName.split("");
+// console.log(lettersArray);
 
-// // 2) Herflere ayrilmish arrayda soyadinizi adinizin onune keciren alqoritm yazin
+// 2) Herflere ayrilmish arrayda soyadinizi adinizin onune keciren alqoritm yazin
 
-// // let fullName = "Nurlan Islamov Nizami";
-// // let lettersArray = fullName.split("");
-// // let fName = lettersArray.slice(7, 14);
-// // let lastName = lettersArray.slice(0, 7);
-// // let surName = lettersArray.slice(15);
+// let fullName = "Nurlan Islamov Nizami";
+// let lettersArray = fullName.split("");
+// let fName = lettersArray.slice(7, 14);
+// let lastName = lettersArray.slice(0, 7);
+// let surName = lettersArray.slice(15);
 
-// // let reversedName = fName.concat(lastName, surName);
+// let reversedName = fName.concat(lastName, surName);
 
-// // console.log(reversedName);
+// console.log(reversedName);
 
-// // 3) Alinan yeni arrayi stringe cevirin ama ilk array deyishmesin(join)
+// 3) Alinan yeni arrayi stringe cevirin ama ilk array deyishmesin(join)
 
-// // let fullName = "Nurlan Islamov Nizami";
-// // let lettersArray = fullName.split("");
-// // let fName = lettersArray.slice(7, 14);
-// // let lastName = lettersArray.slice(0, 7);
-// // let surName = lettersArray.slice(15);
-// // let reversedName = fName.concat(lastName, surName);
-// // let newArrayAsString = reversedName.join('');
-// // console.log(newArrayAsString);
-// // console.log(lettersArray);
+// let fullName = "Nurlan Islamov Nizami";
+// let lettersArray = fullName.split("");
+// let fName = lettersArray.slice(7, 14);
+// let lastName = lettersArray.slice(0, 7);
+// let surName = lettersArray.slice(15);
+// let reversedName = fName.concat(lastName, surName);
+// let newArrayAsString = reversedName.join('');
+// console.log(newArrayAsString);
+// console.log(lettersArray);
 
-// // let arr = [4, 5, 2, 1, 5, 2, 5, 3, 5, 6, 2, 1, 6, 2, 6, 2, 5, 3, 2, 7, 4, 6, 4, 5, 6, 2, 5, 6, 3, 7, 3, 7]
+// let arr = [4, 5, 2, 1, 5, 2, 5, 3, 5, 6, 2, 1, 6, 2, 6, 2, 5, 3, 2, 7, 4, 6, 4, 5, 6, 2, 5, 6, 3, 7, 3, 7]
 
-// // QEYD - Bu tasklarda "arr" arrayindan istifade edilecekdir.....
+// QEYD - Bu tasklarda "arr" arrayindan istifade edilecekdir.....
 
 // let arr = [
 //   4, 5, 2, 1, 5, 2, 5, 3, 5, 6, 2, 1, 6, 2, 6, 2, 5, 3, 2, 7, 4, 6, 4, 5, 6, 2,
@@ -64,17 +64,17 @@
 // repeatedNumbers.sort((a, b) => a - b);
 // console.log(repeatedNumbers);
 
-// //7) arraydaki en boyuk reqemi tapin ve nece defe tekrarlandigini gosterin(Math.max)
+//7) arraydaki en boyuk reqemi tapin ve nece defe tekrarlandigini gosterin(Math.max)
 
-// // let maxNumber = Math.max(...arr);
-// // console.log(maxNumber)
-// // let count = 0;
-// // for (let i = 0; i < arr.length; i++) {
-// //   if (arr[i] === maxNumber) {
-// //     count++;
-// //   }
-// // }
-// // console.log(count);
+// let maxNumber = Math.max(...arr);
+// console.log(maxNumber)
+// let count = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === maxNumber) {
+//     count++;
+//   }
+// }
+// console.log(count);
 
 // 8) Adinizdaki herflerin sayini tapin ve hemin sayin arrayda olub olmamasini yoxlayin Meselen Ulfat adinda 5 herf var ve 5 arrayda var(includes)
 
@@ -130,27 +130,27 @@
 
 // console.log("4 reqeminin indexleri:", indexFour);
 
-// //12) 5 reqeminin arraydaki en boyuk ve en kicik indexlerini tapin
+//12) 5 reqeminin arraydaki en boyuk ve en kicik indexlerini tapin
 
-// // let arr = [4, 5, 2, 1, 5, 2, 5, 3, 5, 6, 2, 1, 6, 2, 6, 2, 5, 3, 2, 7, 4, 6, 4, 5, 6, 2, 5, 6, 3, 7, 3, 7];
+// let arr = [4, 5, 2, 1, 5, 2, 5, 3, 5, 6, 2, 1, 6, 2, 6, 2, 5, 3, 2, 7, 4, 6, 4, 5, 6, 2, 5, 6, 3, 7, 3, 7];
 
-// // let maxIndex = -1;
-// // let minIndex = -1;
+// let maxIndex = -1;
+// let minIndex = -1;
 
-// // for (let i = 0; i < arr.length; i++) {
-// //   if (arr[i] === 5) {
-// //     if (maxIndex === -1 || i > maxIndex) {
-// //       maxIndex = i;
-// //     }
-// //     if (minIndex === -1 || i < minIndex) {
-// //       minIndex = i;
-// //     }
-// //   }
-// // }
-// // console.log("5 regeminin en maximal indexi:", maxIndex);
-// // console.log("5 regeminin en minimal indexi:", minIndex);
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === 5) {
+//     if (maxIndex === -1 || i > maxIndex) {
+//       maxIndex = i;
+//     }
+//     if (minIndex === -1 || i < minIndex) {
+//       minIndex = i;
+//     }
+//   }
+// }
+// console.log("5 regeminin en maximal indexi:", maxIndex);
+// console.log("5 regeminin en minimal indexi:", minIndex);
 
-// //13) "arr" - arrayindan reqemleri 2 den boyuk olan yeni array yaradin ve alinmish arrayla "arr" arrayinin uzunluqlari ferqini hesablayin
+//13) "arr" - arrayindan reqemleri 2 den boyuk olan yeni array yaradin ve alinmish arrayla "arr" arrayinin uzunluqlari ferqini hesablayin
 
 // let arr = [4, 5, 2, 1, 5, 2, 5, 3, 5, 6, 2, 1, 6, 2, 6, 2, 5, 3, 2, 7, 4, 6, 4, 5, 6, 2, 5, 6, 3, 7, 3, 7];
 
@@ -160,102 +160,102 @@
 // let difference = arr.length - newArray.length;
 // console.log("Uzunluq ferqi:", difference);
 
-// //14) 7 reqeminin indexleri cemini tapin.
+//14) 7 reqeminin indexleri cemini tapin.
 
-// // let arr = [4, 5, 2, 1, 5, 2, 5, 3, 5, 6, 2, 1, 6, 2, 6, 2, 5, 3, 2, 7, 4, 6, 4, 5, 6, 2, 5, 6, 3, 7, 3, 7];
+// let arr = [4, 5, 2, 1, 5, 2, 5, 3, 5, 6, 2, 1, 6, 2, 6, 2, 5, 3, 2, 7, 4, 6, 4, 5, 6, 2, 5, 6, 3, 7, 3, 7];
 
-// // let sumOfIndexes = 0;
+// let sumOfIndexes = 0;
 
-// // for (let i = 0; i < arr.length; i++) {
-// //   if (arr[i] === 7) {
-// //     sumOfIndexes += i;
-// //   }
-// // }
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === 7) {
+//     sumOfIndexes += i;
+//   }
+// }
 
-// // console.log("7 reqeminin indexlerinin cemi:", sumOfIndexes);
+// console.log("7 reqeminin indexlerinin cemi:", sumOfIndexes);
 
-let arr2 = [
-  {
-    name: "test",
-    key: 1,
-  },
-  {
-    name: "task",
-    key: 2,
-  },
-  {
-    name: "tanqo",
-    key: 3,
-  },
-  {
-    name: "like",
-    key: 4,
-  },
-  {
-    name: "task",
-    key: 5,
-  },
-  {
-    name: "trust",
-    key: 6,
-  },
-  {
-    name: "test",
-    key: 7,
-  },
-  {
-    name: "last",
-    key: 8,
-  },
-  {
-    name: "tanqo",
-    key: 9,
-  },
-  {
-    name: "elephant",
-    key: 10,
-  },
-  {
-    name: "love",
-    key: 11,
-  },
-  {
-    name: "small",
-    key: 12,
-  },
-  {
-    name: "little",
-    key: 13,
-  },
-];
+// let arr2 = [
+//   {
+//     name: "test",
+//     key: 1,
+//   },
+//   {
+//     name: "task",
+//     key: 2,
+//   },
+//   {
+//     name: "tanqo",
+//     key: 3,
+//   },
+//   {
+//     name: "like",
+//     key: 4,
+//   },
+//   {
+//     name: "task",
+//     key: 5,
+//   },
+//   {
+//     name: "trust",
+//     key: 6,
+//   },
+//   {
+//     name: "test",
+//     key: 7,
+//   },
+//   {
+//     name: "last",
+//     key: 8,
+//   },
+//   {
+//     name: "tanqo",
+//     key: 9,
+//   },
+//   {
+//     name: "elephant",
+//     key: 10,
+//   },
+//   {
+//     name: "love",
+//     key: 11,
+//   },
+//   {
+//     name: "small",
+//     key: 12,
+//   },
+//   {
+//     name: "little",
+//     key: 13,
+//   },
+// ];
 
-// //Aşağıdakı hər bir taskı function icərisində yazın.
+//Aşağıdakı hər bir taskı function icərisində yazın.
 
-// //example task 16 with method///////////////////////////////////
-// // function findT() {
-// //     let newArr = []
-// //     arr2.map((item) => item.name.toLocaleLowerCase().startsWith('t') ? newArr.push(item) : item)
-// //     return newArr
-// // }
+//example task 16 with method///////////////////////////////////
+// function findT() {
+//     let newArr = []
+//     arr2.map((item) => item.name.toLocaleLowerCase().startsWith('t') ? newArr.push(item) : item)
+//     return newArr
+// }
 
-// // console.log(findT());
+// console.log(findT());
 
-// // example: task 16 with for loop////////////////////////////////
-// // function findT2() {
-// //     let newArr = []
-// //     for (let i = 0; i < arr2.length; i++) {
-// //         if (arr2[i].toLocaleLowerCase().startsWith('t')) {
-// //             newArr.push(arr[i])
-// //         }
-// //     }
-// //     return newArr
-// // }
+// example: task 16 with for loop////////////////////////////////
+// function findT2() {
+//     let newArr = []
+//     for (let i = 0; i < arr2.length; i++) {
+//         if (arr2[i].toLocaleLowerCase().startsWith('t')) {
+//             newArr.push(arr[i])
+//         }
+//     }
+//     return newArr
+// }
 
-// // console.log(findT());
+// console.log(findT());
 
-// //QEYD Bu tasklarda arr2 istifade edilecekdir
+//QEYD Bu tasklarda arr2 istifade edilecekdir
 
-// // 16 arr2 de "name"-i "t" herfi ile bashlayan obyektleri yeni arraya yigin
+// 16 arr2 de "name"-i "t" herfi ile bashlayan obyektleri yeni arraya yigin
 
 // function startT(arr) {
 //   return arr.filter(
@@ -267,39 +267,39 @@ let arr2 = [
 
 // 17 arr2 de "name"-i "t" herfi ile bashlayib "t" herfi ile biten obyektlerin sayini tapin
 
-function tt(arr) {
-    return arr.filter(item => item.name.startsWith('t') && item.name.endsWith('t')).length;
-}
+// function tt(arr) {
+//     return arr.filter(item => item.name.startsWith('t') && item.name.endsWith('t')).length;
+// }
 
-const totalCount = tt(arr2);
-console.log(totalCount);
+// const totalCount = tt(arr2);
+// console.log(totalCount);
 
-// // 18 arr2 de "name"-i "t" herfi ile bashlayib "t" herfi ile biten obyektlerin "key"- lerinin cemini tapin
+// 18 arr2 de "name"-i "t" herfi ile bashlayib "t" herfi ile biten obyektlerin "key"- lerinin cemini tapin
 
-// // function tKey(arr) {
-// //     return arr
-// //         .filter(item => item.name.startsWith('t') && item.name.endsWith('t'))
-// //         .reduce((sum, item) => sum + item.key, 0);
-// // }
-// // const totalSum = tKey(arr2);
-// // console.log(totalSum);
+// function tKey(arr) {
+//     return arr
+//         .filter(item => item.name.startsWith('t') && item.name.endsWith('t'))
+//         .reduce((sum, item) => sum + item.key, 0);
+// }
+// const totalSum = tKey(arr2);
+// console.log(totalSum);
 
-// // 19 arr2 de "name"-i "e" herfi ile biten obyeklerdeki name-in deyerini "SuperDev" sozu ile evezleyin.
+// 19 arr2 de "name"-i "e" herfi ile biten obyeklerdeki name-in deyerini "SuperDev" sozu ile evezleyin.
 
-// // function eBiten(arr) {
-// //   return arr.map((item) => ({
-// //     ...item,
-// //     name:
-// //       item.name.charAt(item.name.length - 1).toLowerCase() === "e"
-// //         ? "SuperDev"
-// //         : item.name,
-// //   }));
-// // }
-// // const newArray = eBiten(arr2);
+// function eBiten(arr) {
+//   return arr.map((item) => ({
+//     ...item,
+//     name:
+//       item.name.charAt(item.name.length - 1).toLowerCase() === "e"
+//         ? "SuperDev"
+//         : item.name,
+//   }));
+// }
+// const newArray = eBiten(arr2);
 
-// // console.log(newArray);
+// console.log(newArray);
 
-// // 20 arr2 de "name"-i en uzun olan obyektin key-i ni tapin
+// 20 arr2 de "name"-i en uzun olan obyektin key-i ni tapin
 
 // function uzunKeyTap(arr) {
 //     return arr.reduce((acc, item) => (item.name.length > acc.name.length ? item : acc)).key;
@@ -317,38 +317,38 @@ console.log(totalCount);
 // const squareIndex = indexSquare(arr2);
 // console.log(squareIndex);
 
-// // 22 arr2 de "name"-inin uzunlugu 4 olan obyektlerden ibaret yeni array yaradin.
+// 22 arr2 de "name"-inin uzunlugu 4 olan obyektlerden ibaret yeni array yaradin.
 
-// // function fourLetters(arr) {
-// //     return arr.filter(item => item.name.length === 4);
-// // }
-// // const newArray = fourLetters(arr2);
-// // console.log(newArray);
+// function fourLetters(arr) {
+//     return arr.filter(item => item.name.length === 4);
+// }
+// const newArray = fourLetters(arr2);
+// console.log(newArray);
 
-// // 23 arr2 de en boyuk "key" - i olan obyektin "name"-i ni tapin
+// 23 arr2 de en boyuk "key" - i olan obyektin "name"-i ni tapin
 
-// // function findNameOfMaxKey(arr) {
-// //     const maxKeyItem = arr.reduce((acc, item) => (item.key > acc.key ? item : acc));
-// //     return maxKeyItem.name;
-// // }
-// // const nameOfMaxKey = findNameOfMaxKey(arr2);
-// // console.log(nameOfMaxKey);
+// function findNameOfMaxKey(arr) {
+//     const maxKeyItem = arr.reduce((acc, item) => (item.key > acc.key ? item : acc));
+//     return maxKeyItem.name;
+// }
+// const nameOfMaxKey = findNameOfMaxKey(arr2);
+// console.log(nameOfMaxKey);
 
-// // 24 arr2 de terkibinde 2 'L' herfi olan obyekt(ler)in index(ler)ini tapin.
+// 24 arr2 de terkibinde 2 'L' herfi olan obyekt(ler)in index(ler)ini tapin.
 
-// // const ikiLİndex = [];
-// // for (let i = 0; i < arr2.length; i++) {
-// //   if (arr2[i].name.toLowerCase().split('l').length - 1 === 2) {
-// //     ikiLİndex.push(i);
-// //   }
-// // }
+// const ikiLİndex = [];
+// for (let i = 0; i < arr2.length; i++) {
+//   if (arr2[i].name.toLowerCase().split('l').length - 1 === 2) {
+//     ikiLİndex.push(i);
+//   }
+// }
 
-// // console.log("2 'L' herfi olan indexleri:", ikiLİndex);
+// console.log("2 'L' herfi olan indexleri:", ikiLİndex);
 
-// // 25 arr2 de terkibinde 2 't' herfi olan obyekt(ler)in key(ler)ini tapin.
-// // function findKeysWithDoubleT(arr) {
-// //     return arr
-// //         .filter(item => (item.name.match(/t/gi) || []).length === 2)
-// //         .map(item => item.key);}
-// //         const keysWithDoubleT = findKeysWithDoubleT(arr2);
-// // console.log(keysWithDoubleT);
+// 25 arr2 de terkibinde 2 't' herfi olan obyekt(ler)in key(ler)ini tapin.
+// function findKeysWithDoubleT(arr) {
+//     return arr
+//         .filter(item => (item.name.match(/t/gi) || []).length === 2)
+//         .map(item => item.key);}
+//         const keysWithDoubleT = findKeysWithDoubleT(arr2);
+// console.log(keysWithDoubleT);
